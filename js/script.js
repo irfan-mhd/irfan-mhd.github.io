57f92c884aee2fe7f49d0082aded7d0f021cd9f3
irfan-mhd <irfanuiux9847@gmail.com git config --global user.name irfan-mhd git config --global user.name irfan-mhdgit config --global user.email irfanuiux9847@gmail.com>
2025-08-01 11:59:01 -1200

    day-12

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ $('.toggle-bt').click(function(){
     $('nav ul').toggleClass('clicked')
     
 })
+$(window).on('scroll',function(){
+    if($(window).scrolltop() > 50){
+        $('header').css('background-color','yellow')
+    }else{
+        $('header').css('background-color','none');
+    }
+});
